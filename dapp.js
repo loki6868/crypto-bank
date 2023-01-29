0ce2ac22e757cd153c1b5235b4f32a39362a10c7
loki6868 <optimuscosmetic@gmail.com>
2023-01-29 18:08:31 +0700

    staking platform

--- dapp.js
+++ dapp.js
@@ -1,56 +1,92 @@
 $(document).ready(function () {
-    ethereum.on('accountsChanged', function (accounts) {
-        // Time to reload your interface with accounts[0]!
-        alert("1")
-    });
-    checkMM();
-    const stakingAppAddress = "0x54320Ff2d9c471DC936982e3184aC3917eAA7540";
     const web3 = new Web3(window.ethereum);
     var currentAccount;
-    // window.ethereum.enable();
-    var stakingAppContract = new web3.eth.Contract(stakingAppABI, stakingAppAddress);
+    var currentSelectedAsset;
+    var currentSelectedAssetBalance = 1;
+    const stakingAppContract = new web3.eth.Contract(stakingAppABI, stakingAppAddress);
 
-    var ids = getPositionIdByAddress().then(data => {
-        console.log(data)
-    }).catch(error => {
-        console.log(error);
+    displayMarkets();
+    checkMM();
+    onload();
+
+    ethereum.on('accountsChanged', function (accounts) {
+        onload();
     });
 
-    //GetPositsionsByAddress
-
-    //Stake
-
-    //Claim
-
-    //Widthdraw
-
-    //Switch Accounts
-
-    //Switch Networks
-
-    //var token = getToken('ETH').then(data => { console.log(data) });
-    //stake('USDT', 10);
-
-    $("#connectMM").click(function () {
-        connectMM().then(data => {
-            currentAccount = data[0];
-            console.log(currentAccount);
-            getPositionIdByAddress().then(data => {
-                console.log(data)
+    function onload() {
+        connectMM().then(accounts => {
+            currentAccount = accounts[0];
+            displayConnectedWallet();
+            getPositionIdsByAddress(currentAccount).then(ids => {
+                $('#staked-assets--loading').text('loading...');
+                displayStakedAssets(ids).then(data => {
+                    setTimeout(myGreeting, 2000);
+                });
             }).catch(error => {
                 console.log(error);
             });
-
         }).catch(error => {
-            console.log(error);
+            console.log("MM ERROR " + error);
         })
+    }
+
+    function displayMarkets() {
+        $('#ethereumMarket').empty();
+        let index = 1;
+        for (token of tokens) {
+            let tokenDetail = token[1];
+            $('#ethereumMarket').append(`<tr>
+                <th scope="row">${index++}</th>
+                <td>
+                    <img src="${tokenDetail.imgSrc}" height="40px" alt="">
+                    <b>${tokenDetail.name}</b>
+                </td>
+                <td class='fw-bold'>${tokenDetail.symbol}</td>
+                <td class='fw-bold'>${tokenDetail.rate}</td>
+                <td>
+                    <button type="button" class="btn btn-primary btnStake" data-bs-toggle="modal" data-bs-target="#myModal" value="${tokenDetail.symbol}">
+                        Stake now
+                    </button>
+                </td>
+            </tr>`);
+        }
+    };
+
+    $("#connectMM").click(function () {
+        if (currentAccount === 'undefined') {
+            onload();
+        } else {
+            console.log('is connected')
+        }
+    });
+
+    $('.btnStake').click(async function () {
+        currentSelectedAsset = $(this).val();
+        $('#modal-img-src').attr('src', tokens.get(currentSelectedAsset).imgSrc)
+        $('#symbol').text(currentSelectedAsset);
+        await getBalance().then(amount => {
+            currentSelectedAssetBalance = web3.utils.fromWei(amount + '', 'ether');
+        }).catch(console.log);
+        $('#amount').attr('placeholder', currentSelectedAssetBalance);
+    });
+
+    $('#modal-stake-btnMaximum').click(async function () {
+        $('#amount').val(currentSelectedAssetBalance);
+    });
+
+    $('#btn-stakeModal').click(function () {
+        // var amountMax = currentSelectedAssetBalance;
+        var amount = $('#amount').val();
+        if (amount <= 0) {
+            console.log("amount <= 0")
+        } else {
+            stake(currentSelectedAsset, amount);
+        }
     });
 
     async function connectMM() {
         const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
         return accounts;
-        // const account = accounts[0];
-        // showAccount.innerHTML = account;
     }
 
     function checkMM() {
@@ -61,25 +97,171 @@ $(document).ready(function () {
         }
     }
 
-    function getToken(_symbol) {
-        return stakingAppContract.methods.getToken(_symbol).call();
+    function displayConnectedWallet() {
+        $('#connectMM span').text(currentAccount.substr(0, 6) + '...' + currentAccount.substr(-6));
     }
+
+    async function displayStakedAssets(ids) {
+        $('#stakedAssets').empty();
+        let index = 1;
+        await (async () => {
+            for await (id of ids) {
+                await caculateRewardsByPositionId(id).then(reward => {
+                    var reward = web3.utils.fromWei(reward + '', 'ether');
+                    getPositionById(id).then(position => {
+                        $('#stakedAssets').append(`<tr>
+                        <th scope="row">${index++}</th>
+                        <td>
+                            <img src="${tokens.get(position.symbol).imgSrc}" height="40px" alt="">
+                            <b>${position.symbol}</b>
+                        </td>
+                        <td class='fw-bold'>${web3.utils.fromWei(position.amount + '', 'ether')}</td>
+                        <td class='fw-bold'>${tokens.get(position.symbol).rate}</td>
+                        <td class='fw-bold'>${reward}</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btnClaim" value="${position.positionId}">
+                                Claim
+                            </button>
+                        </td>
+                        <td>
+                          <button type="button" class="btn btn-primary btnWithdraw" data-bs-toggle="modal" data-bs-target="#msgWithdraw" value="${position.positionId}">
+                            Withdraw
+                          </button>
+                        </td>
+                      </tr>`);
+                    })
+                }).catch(error => {
+                    console.log("caculateRewardsByPositionId Fail");
+                });
+            }
+        })();
+    };
+
+    function myGreeting() {
+        $('#staked-assets--loading').empty();
+        $('.btnClaim').click(function () {
+            console.log('position id ' + $(this).val())
+            claim($(this).val());
+        });
+
+        $('.btnWithdraw').click(function () {
+            if (true) {
+                console.log('Withdraw no yet!')
+            } else {
+                withdraw($(this).val());
+            }
+        });
+    };
 
     async function stake(_symbol, _amount) {
-        var usdtContract = new web3.eth.Contract(usdtABI, '0xFdb7EdaE5218a5f0E9F5860c71AdE015209385B0');
-        var toWei = web3.utils.toWei(_amount+'', 'ether')
-        await usdtContract.methods.approve('0x54320Ff2d9c471DC936982e3184aC3917eAA7540', toWei).send({ from: '0x348E9aC371Ee4524f78bA679d81484d98E9eb632' });
-
-        stakingAppContract.methods.stake(_symbol, toWei).send({ from: '0x348E9aC371Ee4524f78bA679d81484d98E9eb632' });
+        if (_symbol == 'ETH') {
+            var toWei = web3.utils.toWei(_amount + '', 'ether');
+            stakingAppContract.methods.stake(_symbol, toWei)
+                .send({ from: currentAccount, value: toWei })
+                .on("transactionHash", function (hash) {
+                    $('#msgAlert').text('Transaction in progress ...⚙️');
+                    $(".alert").show('medium');
+                })
+                .on("receipt", function (receipt) {
+                    $(".alert").hide('medium');
+                    $('#msgAlert').text('Successful Transaction!');
+                    $(".alert").show('medium');
+                    setTimeout(function () {
+                        $(".alert").hide('medium');
+                        location.reload(true);
+                    }, 2000);
+                })
+                .on("error", function (error) {
+                    $(".alert").hide('medium');
+                    $('#msgAlert').text('Transaction Failed!');
+                    $(".alert").show('medium');
+                    setTimeout(function () {
+                        $(".alert").hide('medium');
+                    }, 5000);
+                    console.log("Stake error" + error);
+                });
+        } else {
+            var tokenContract = new web3.eth.Contract(tokens.get(currentSelectedAsset).abi, tokens.get(currentSelectedAsset).address);
+            var toWei = web3.utils.toWei(_amount + '', 'ether');
+            await tokenContract.methods.approve(stakingAppAddress, toWei).send({ from: currentAccount });
+            stakingAppContract.methods.stake(_symbol, toWei)
+                .send({ from: currentAccount })
+                .on("transactionHash", function (hash) {
+                    $('#msgAlert').text('Transaction in progress ...⚙️');
+                    $(".alert").show('medium');
+                })
+                .on("receipt", function (receipt) {
+                    $(".alert").hide('medium');
+                    $('#msgAlert').text('Successful Transaction.');
+                    $(".alert").show('medium');
+                    setTimeout(function () {
+                        $(".alert").hide('medium');
+                        location.reload(true);
+                    }, 2000);
+                })
+                .on("error", function (error) {
+                    $(".alert").hide('medium');
+                    $('#msgAlert').text('Transaction Failed.');
+                    $(".alert").show('medium');
+                    setTimeout(function () {
+                        $(".alert").hide('medium');
+                    }, 5000);
+                    console.log("Stake error" + error);
+                });
+        }
     }
 
-    function getPositionIdByAddress() {
-        return stakingAppContract.methods.getAddressToPositionIds().call({ from: '0x348E9aC371Ee4524f78bA679d81484d98E9eb632' });
+    function getPositionIdsByAddress(address) {
+        return stakingAppContract.methods.getPositionIdsByAddress(address).call();
     }
 
     function getPositionById(id) {
-        return cryptoZombies.methods.zombies(id).call();
+        return stakingAppContract.methods.getPositionById(id).call();
+    }
+
+    function caculateRewardsByPositionId(id) {
+        return stakingAppContract.methods.caculateRewards(id).call();
+    }
+
+    function claim(positionId) {
+        return stakingAppContract.methods.claim(positionId)
+            .send({ from: currentAccount })
+            .on("transactionHash", function (hash) {
+                $('#msgAlert').text('Transaction in progress ...⚙️');
+                $(".alert").show('medium');
+            })
+            .on("receipt", function (receipt) {
+                $(".alert").hide('medium');
+                $('#msgAlert').text('Successful Transaction!');
+                $(".alert").show('medium');
+                setTimeout(function () {
+                    $(".alert").hide('medium');
+                    location.reload(true);
+                }, 2000);
+            })
+            .on("error", function (error) {
+                $(".alert").hide('medium');
+                $('#msgAlert').text('Transaction Failed!');
+                $(".alert").show('medium');
+                setTimeout(function () {
+                    $(".alert").hide('medium');
+                }, 5000);
+                console.log("Claim error" + error);
+            });
+    }
+
+    function withdraw(positionId) {
+        return stakingAppContract.methods.unstake(positionId).send({ from: currentAccount });
+    }
+
+    async function getBalance() {
+        if (currentSelectedAsset !== 'ETH') {
+            const tokenContract = new web3.eth.Contract(tokens.get(currentSelectedAsset).abi, tokens.get(currentSelectedAsset).address);
+            var balance = await tokenContract.methods.balanceOf(currentAccount).call();
+            return balance;
+        } else {
+            var balance = await web3.eth.getBalance(currentAccount);
+            return balance;
+        }
     }
 });
-
-// add param addess to function getPositon
--- stakingAppABI.js
+++ stakingAppABI.js
@@ -1,3 +1,4 @@
+const stakingAppAddress = "0x8d08d84266ec02292713b98eBEE40a2D08e6aC7a";
 const stakingAppABI = [
 	{
 		"inputs": [
@@ -56,35 +57,6 @@ const stakingAppABI = [
 	{
 		"inputs": [
 			{
-				"internalType": "string",
-				"name": "_symbol",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_amount",
-				"type": "uint256"
-			}
-		],
-		"name": "stake",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [],
-		"name": "claimed",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
 				"internalType": "uint256",
 				"name": "_positionId",
 				"type": "uint256"
@@ -101,69 +73,66 @@ const stakingAppABI = [
 		"type": "function"
 	},
 	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
 		"anonymous": false,
 		"inputs": [
 			{
-				"components": [
-					{
-						"internalType": "uint256",
-						"name": "positionId",
-						"type": "uint256"
-					},
-					{
-						"internalType": "address",
-						"name": "stakerAddress",
-						"type": "address"
-					},
-					{
-						"internalType": "string",
-						"name": "symbol",
-						"type": "string"
-					},
-					{
-						"internalType": "uint256",
-						"name": "rate",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "amount",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "createdTime",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "claimedTime",
-						"type": "uint256"
-					},
-					{
-						"internalType": "bool",
-						"name": "enableClaim",
-						"type": "bool"
-					},
-					{
-						"internalType": "bool",
-						"name": "enableUnstake",
-						"type": "bool"
-					},
-					{
-						"internalType": "bool",
-						"name": "unstaked",
-						"type": "bool"
-					}
-				],
 				"indexed": false,
-				"internalType": "struct StakingApp.Position",
+				"internalType": "uint256",
 				"name": "",
-				"type": "tuple"
+				"type": "uint256"
 			}
 		],
-		"name": "staked",
+		"name": "NewClaim",
 		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "NewStaking",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "NewUnstaking",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_symbol",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "stake",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
 	},
 	{
 		"inputs": [
@@ -177,19 +146,6 @@ const stakingAppABI = [
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "unstaked",
-		"type": "event"
 	},
 	{
 		"inputs": [
@@ -296,19 +252,6 @@ const stakingAppABI = [
 		"type": "function"
 	},
 	{
-		"inputs": [],
-		"name": "getAddressToPositionIds",
-		"outputs": [
-			{
-				"internalType": "uint256[]",
-				"name": "",
-				"type": "uint256[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
 		"inputs": [
 			{
 				"internalType": "uint256",
@@ -374,6 +317,25 @@ const stakingAppABI = [
 				"internalType": "struct StakingApp.Position",
 				"name": "",
 				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_stakerAddress",
+				"type": "address"
+			}
+		],
+		"name": "getPositionIdsByAddress",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
 			}
 		],
 		"stateMutability": "view",
@@ -712,3 +674,571 @@ const usdtABI = [
 		"type": "function"
 	}
 ]
+const wbtcABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "subtractedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "decreaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "addedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "increaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+const xrpABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "subtractedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "decreaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "addedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "increaseAllowance",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+const tokenABIs = new Map([
+	['USDT',usdtABI],
+])
+const tokens = new Map([
+	['USDT', { name: 'Tether', symbol: 'USDT', imgSrc: 'images/token/usdt.png', rate: '4-10%', address: '0xFdb7EdaE5218a5f0E9F5860c71AdE015209385B0', abi: usdtABI }],
+	['USDC', { name: 'USD Coin', symbol: 'USDC', imgSrc: 'images/token/usdc.svg', rate: '4-10%', address: '0xDc045fd2605AF9B904599010EE27a27c3Afec69a', abi: wbtcABI }],
+	['BUSD', { name: 'BUSD', symbol: 'BUSD', imgSrc: 'images/token/busd.png', rate: '4-10%' }],
+	['ETH', { name: 'Ethereum', symbol: 'ETH', imgSrc: 'images/token/eth.png', rate: '5-12%' }],
+	['wBTC', { name: 'Wrapped Bitcoin', symbol: 'wBTC', imgSrc: 'images/token/btc.png', rate: '4-8%' }],
+	['BNB', { name: 'BNB', symbol: 'BNB', imgSrc: 'images/token/bnb.svg', rate: '7-15%' }],
+	['DAI', { name: 'Dai', symbol: 'DAI', imgSrc: 'images/token/dai.png', rate: '5-10%' }],
+	['XRP', { name: 'Ripple', symbol: 'XRP', imgSrc: 'images/token/xrp.png', rate: '8-17%', address: '0x3c800AB31518a5B4fC8f65dE48669Bc097963A05', abi: xrpABI }]
+]);
